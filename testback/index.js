e52c601951b2396c5954037d9ef797b4aea962f1
Nitin Sabale <nitinsabale59@gmail.com>
2020-05-01 17:40:39 +0530

    Testing middleware

--- testback/index.js
+++ testback/index.js
@@ -1,21 +1,39 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/signup",(req,res) => {
-    return res.send("You are ready to signup!");
+app.get("/signup", (req, res) => {
+  return res.send("You are ready to signup!");
 });
 
-app.get("/login",(req,res) => {
-    return res.send("You are ready to login!");
+const admin = (req, res) => {
+  return res.send("this is admin dashboard");
+};
+
+
+const isLoggedIn = (req,res, next) => {
+    console.log("User has logged in");
+    next();
+}
+
+const isAdmin = (req,res, next) => {
+    console.log("Admin is running");
+    next();
+}
+
+
+app.get("/admin",isLoggedIn, isAdmin, admin);
+
+app.get("/login", (req, res) => {
+  return res.send("You are ready to login!");
 });
 
-app.get("/user",(req,res) => {
-    return res.send("You are a new user!");
+app.get("/user", (req, res) => {
+  return res.send("You are a new user!");
 });
 
 app.listen(port, () => {
-    console.log("Server is running");
+  console.log("Server is running");
 });
 // const port = 3000
 
