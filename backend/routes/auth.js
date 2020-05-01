6c809d37d165a5fccb02d4f030fc1067c3516231
Nitin Sabale <nitinsabale59@gmail.com>
2020-05-02 00:31:54 +0530

    validation and routes for signin

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,29 @@
 var express = require("express");
 var router = express.Router();
 
-const { signOut, signUp } = require("../controllers/auth");
+const { check, validationResult } = require("express-validator");
+const { signOut, signUp, signIn } = require("../controllers/auth");
 
-router.post("/signup", signUp)
+router.post(
+  "/signup",
+  [
+    check("name", "Name should be atleast 3 char").isLength({ min: 3 }),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password should be 3 char").isLength({ min: 3 }),
+  ],
+  signUp
+);
+
+router.post(
+    "/signin",
+    [
+      check("email", "Email is required").isEmail(),
+      check("password", "Password field is required").isLength({ min: 1 }),
+    ],
+    signIn
+  );
+
+
 router.get("/signout", signOut);
 
 module.exports = router;
